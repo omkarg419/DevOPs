54236ec104ceb9c9988940284a0d129de56cd63f
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-06-09 14:12:29 +0530

    fix: update health check message to correct exclamation marks

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-	res.json({ message: "Server is Healthy v4.5" ,status: "GitHub Actions is working fine!" });
+	res.json({ message: "Server is Healthy v4.5" ,status: "GitHub Actions is working fine!!!!!" });
 });
 
 const PORT = process.env.PORT ?? 8000;
